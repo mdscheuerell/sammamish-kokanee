50f9fbffa7c40e583cb53947c60d02c6230667e1
Mark Scheuerell <scheuerl@uw.edu>
2023-01-25 15:29:38 -0800

    added simple PVA to analysis

--- R/trend_analysis.R
+++ R/trend_analysis.R
@@ -27,7 +27,7 @@ mod_list <- list(
   Q = matrix("q"),
   Z = matrix(1),
   A = matrix(0),
-  R = matrix("r")
+  R = matrix(1.5) # matrix("r")
 )
 
 mod_fit_unbiased <- MARSS(y = matrix(log_counts, nrow = 1),
@@ -42,7 +42,7 @@ mod_list <- list(
   Q = matrix("q"),
   Z = matrix(1),
   A = matrix(0),
-  R = matrix("r")
+  R = matrix(1.5) # matrix("r")
 )
 
 mod_fit_biased <- MARSS(y = matrix(log_counts, nrow = 1),
@@ -60,9 +60,51 @@ mod_list <- list(
   Q = matrix("q"),
   Z = matrix(1),
   A = matrix(0),
-  R = matrix("r")
+  R = matrix(1.5) # matrix("r")
 )
 
 mod_fit_ar1 <- MARSS(y = matrix(log_counts, nrow = 1),
                      model = mod_list, control = control_list)
+
+
+#### Popn viability analysis ####
+
+## set seed for random numbers
+
+## number of MC simulations
+n_sims <- 1000
+
+## time horizon
+n_years <- 50
+
+## quasi-extinction threshold
+qet <- log(10)
+
+## initial state = log(count) in 2022
+x0 <- log_counts[length(log_counts)]
+
+## future population vector
+# popn_vec <- c(x0, rep(NA, n_years))
+popn_mat <- matrix(c(x0, rep(NA, n_years)),
+                   nrow = n_sims, ncol = n_years + 1,
+                   byrow = TRUE)
+
+## simulations for unbiased
+for(i in 1:n_sims) {
+  for(t in 2:(n_years + 1)) {
+    popn_mat[i, t] <- popn_mat[i, t - 1] + rnorm(1, 0, sqrt(mod_fit_unbiased$par$Q))
+  }
+}
+
+pva_unbiased <- apply(popn_mat[, -1] < qet, 2, sum) / n_sims
+
+
+## simulations for biased
+for(i in 1:n_sims) {
+  for(t in 2:(n_years + 1)) {
+    popn_mat[i, t] <- popn_mat[i, t - 1] + mod_fit_biased$par$U + rnorm(1, 0, sqrt(mod_fit_biased$par$Q))
+  }
+}
+
+pva_biased <- apply(popn_mat[, -1] < qet, 2, sum) / n_sims
 
